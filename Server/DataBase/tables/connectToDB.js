a5e57aa0bf683d68106b7c34aa32b7b86785a8b5
naomi1877 <naomi0556751877@gmail.com>
2024-08-11 17:52:56 +0300

    change in the server

--- Server/DataBase/tables/connectToDB.js
+++ Server/DataBase/tables/connectToDB.js
@@ -7,6 +7,9 @@ var conDB = mysql.createConnection({
   database: "schedule_db",
 });
 
+
+
+
 conDB.connect(function (err) {
   if (err) throw err;
   console.log("Connected to schedule_db succied");//**
