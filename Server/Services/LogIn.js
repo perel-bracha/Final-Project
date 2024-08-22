01d6c6e4b728f13c227136beea90c159c0b25a51
פרל ברכה נדל <pn0556781949@gmail.com>
2024-08-22 19:52:16 +0300

    LogIn services

--- Server/Services/LogIn.js
+++ Server/Services/LogIn.js
@@ -1,12 +1,21 @@
 const jwt = require("jsonwebtoken");
 const conDB = require("../DataBase/tables/connectToDB");
+// const secretKey = process.env.JWT_SECRET_KEY; // השתמשי במפתח סודי מוגדר מראש
 
-function LogIn(password) {
-  let readQuery = `SELECT ID FROM employee WHERE ID=${password}`;
+async function LogIn(password) {
+  let readQuery = `SELECT ID FROM employee WHERE ID=?`;
 
-  conDB.query(readQuery, (error, result) => {
-    callBack(error, result, resToCallBack);
-  });
+  const [rows] = await conDB.execute(readQuery, [password]);
+  const user=rows[0];
+  ///בדיקת אימות
+  if(!user){
+    ////////שגיאה איך?
+  }
+
+  else{
+    const token = jwt.sign({ userId: user.ID }, secretKey, { expiresIn: '1h' });
+    return token;
+  }
 }
 
 module.exports = LogIn;
