a7a757a2599109874db5f1b5df5b01e55df599f5
naomi1877 <naomi0556751877@gmail.com>
2024-08-28 18:54:26 +0300

    Token complete! bravo

--- Server/API/unitAPI.js
+++ Server/API/unitAPI.js
@@ -12,16 +12,31 @@ app.get("/units", (req, res) => {
   Read("unit", { UnitId: req.query.UnitId }, callBack, res);
 });
 
-app.post("/units", (req, res) => {
-  Insert("unit", req.body.newUnit, callBack, res);
-});
+app.post(
+  "/units",
+  verifyToken,
+  checkPermissions(["Admin"], "insert"),
+  (req, res) => {
+    Insert("unit", req.body.newUnit, callBack, res);
+  }
+);
 
-app.put("/units", (req, res) => {
-  Update("unit", req.body.unitToUpdate, callBack, res);
-});
+app.put(
+  "/units",
+  verifyToken,
+  checkPermissions(["Admin"], "update"),
+  (req, res) => {
+    Update("unit", req.body.unitToUpdate, callBack, res);
+  }
+);
 
-app.delete("/units", (req, res) => {
-  Delete("unit", "UnitId", req.query.id, callBack, res);
-});
+app.delete(
+  "/units",
+  verifyToken,
+  checkPermissions(["Admin"], "delete"),
+  (req, res) => {
+    Delete("unit", "UnitId", req.query.id, callBack, res);
+  }
+);
 
 module.exports = app;
