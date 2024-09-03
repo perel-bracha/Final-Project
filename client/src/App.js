83c57af1c665fd407593e64a3fc3dfaa44c09fd0
naomi1877 <naomi0556751877@gmail.com>
2024-09-03 14:01:08 +0300

    marge the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import TeacherForm from "./pages/TeacherForm";
+import AddTeacher from "./components/AddTeacher";
+import AddUpdateSpe from "./components/AddUpdateSpe";
+import AddCourse from "./components/AddCourse";
+import AddTeam from "./components/AddTeam";
+import Hello from "./pages/Hello";
 
 function App() {
   return (
+    // <div className="App">
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="hello" element={<Hello />} />
+
+    //     {/* <Route path="home"> */}
+    //     {/* <Route index element={<Home />} /> */}
+    //     <Route path="addTeacher" element={<AddTeacher />} />
+    //     <Route path="addSpe" element={<AddUpdateSpe />} />
+    //     <Route path="addTeam" element={<AddTeam />} />
+    //     <Route path="addCourse" element={<AddCourse />} />
+    //     {/* </Route> */}
+
+    //     <Route path="teacher" element={<TeacherForm />} />
+    //   </Routes>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="hello/*" element={<Hello />} />
+        
+        <Route path="teacher" element={<TeacherForm />} />
+      </Routes>
     </div>
   );
 }
