b7c317805d48d76d08edbb581ab2c3daef9b0ac7
naomi1877 <naomi0556751877@gmail.com>
2024-12-04 16:13:40 +0200

    entry

--- Server/API/scheduleAPI.js
+++ Server/API/scheduleAPI.js
@@ -11,7 +11,7 @@ const app = Router();
 app.get(
   "/schedules",
   verifyToken,
-  checkPermissions(["Coordinator", "Admin"], "read"),
+  checkPermissions(["Teacher", "Coordinator", "Admin"], "read"),
   (req, res) => {
     Read(
       "schedule",
